c9a6017faa49957686b2726294e57bf40d6a22d8
Alik Khilazhev <alikhil@mail.ru>
2021-08-12 09:31:13 +0300

    ci: change repo structure

---
+++ .ci/scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eou pipefail
+
+dirs=$(git tag -l --sort=v:refname | tail -n2 | xargs -L2 git diff --name-only | xargs -L1 dirname | grep images | uniq)
+
+while IFS= read -r dir; do
+	cd "$dir"
+
+	image=$(basename "$(dirname "$dir")")
+	tag=$(basename "$dir")
+
+	docker build --tag "ghcr.io/kazanexpress/$image:$tag" --tag "ghcr.io/kazanexpress/$image:latest" .
+	docker push --all-tags ghcr.io/kazanexpress/$image
+
+	cd - >/dev/null
+done <<<"$dirs"
