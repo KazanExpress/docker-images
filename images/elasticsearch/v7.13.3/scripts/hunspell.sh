5aae4825445d208d7fd6f33eace57577262e5863
Igor Ermolaev <ermolaev_ig@evrone.com>
2022-04-28 21:03:23 +1000

    uzbek hunspell dictionaries for elastic search

--- images/elasticsearch/v7.13.3/scripts/hunspell.sh
+++ images/elasticsearch/v7.13.3/scripts/hunspell.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-BASE_DIR=config/hunspell
+#BASE_DIR=config/hunspell
+BASE_DIR=/home/igor/temp/hunspell
 BASE_URL="https://raw.githubusercontent.com/LibreOffice/dictionaries/master"
 
 # english
@@ -12,3 +13,15 @@ wget --directory-prefix=$BASE_DIR/en_US \
 wget --directory-prefix=$BASE_DIR/ru_RU \
 	${BASE_URL}/ru_RU/ru_RU.aff \
 	${BASE_URL}/ru_RU/ru_RU.dic
+
+BASE_URL="https://github.com/u2b3k/uz-hunspell/blob/master"
+
+# uzbek lat
+wget --directory-prefix=$BASE_DIR/uz_UZ \
+	${BASE_URL}/uz-lat.aff \
+	${BASE_URL}/uz-lat.dic
+
+# uzbek cyr
+wget --directory-prefix=$BASE_DIR/uz_RU \
+	${BASE_URL}/uz-cyr.aff \
+	${BASE_URL}/uz-cyr.dic
