914a26f811e605e48047147d1e65fadc48d71384
Pavel Dashkevich <64554439+paulDashkevich@users.noreply.github.com>
2022-09-01 10:34:20 +0300

    feat(clicklogs): up version (#78)

---
+++ images/clickhouse-log-exporter/1.1.0/utils/clickhouse-client.mjs
@@ -0,0 +1,124 @@
+import _ from 'lodash';
+import { Transform } from 'stream';
+
+import { Session, serviceClients } from '@yandex-cloud/nodejs-sdk';
+import {
+  StreamClusterLogsRequest,
+  StreamClusterLogsRequest_ServiceType,
+  StreamLogRecord,
+} from '@yandex-cloud/nodejs-sdk/dist/generated/yandex/cloud/mdb/clickhouse/v1/cluster_service.js';
+
+export class ClickhouseClient {
+  /**
+   * @param {Session} session
+   */
+  constructor(session, clusterId) {
+    this.clusterId = clusterId;
+    this.session = session;
+    this.clusterService = this.session.client(
+      serviceClients.ClickHouseClusterServiceClient
+    );
+  }
+
+  /**
+   *
+   * @param {Partial<StreamClusterLogsRequest>} opts
+   * @returns
+   */
+  streamLogs(opts) {
+    return this.clusterService.streamLogs(
+      StreamClusterLogsRequest.fromPartial({
+        clusterId: this.clusterId,
+        serviceType: StreamClusterLogsRequest_ServiceType.CLICKHOUSE,
+        ...opts,
+      })
+    );
+  }
+
+  /**
+   * @param {AsyncIterable<StreamLogRecord>} stream
+   */
+  async *getProcessMessageIterator(stream) {
+    for await (const msg of stream) {
+      const record = {
+        ...msg.record,
+        message: {
+          ...msg.record.message,
+          severity: this._remapSeverity(msg.record.message.severity),
+        },
+      };
+
+      yield {
+        nextRecordToken: msg.nextRecordToken,
+        clusterId: this.clusterId,
+        ...record,
+      };
+    }
+  }
+
+  _remapSeverity(current) {
+    const remap = {
+      Information: 'info',
+      Warning: 'warn',
+    };
+
+    const remapped = remap[current] || current;
+    return remapped.toLowerCase();
+  }
+}
+
+class BatchStream extends Transform {
+  constructor(batchSize = 10, batchSendTimeout = 10000) {
+    super({
+      objectMode: true,
+    });
+
+    this.batchSize = batchSize;
+    this.batchSendTimeout = batchSendTimeout;
+    this.buffer = [];
+  }
+
+  _transform(chunk, _encoding, callback) {
+    this.buffer.push(chunk);
+
+    if (this.buffer.length >= this.batchSize) {
+      this.push(this.makeBatch());
+    }
+
+    this.setTimeout();
+
+    callback();
+  }
+
+  _flush(callback) {
+    clearTimeout(this.timeout);
+    if (this.buffer.length > 0) {
+      this.push(this.makeBatch());
+    }
+    callback();
+  }
+
+  setTimeout() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      if (this.buffer.length > 0) {
+        this.push(this.makeBatch());
+      } else {
+        console.debug('no messages to send');
+      }
+    }, this.batchSendTimeout);
+  }
+
+  makeBatch() {
+    const max = _.maxBy(this.buffer, (r) => parseInt(r.nextRecordToken, 10));
+    const batch = { max: max.nextRecordToken, buffer: [...this.buffer] };
+
+    this.buffer = [];
+
+    return batch;
+  }
+}
+
+ClickhouseClient.getBatchStream = function (batchSize = 10, timeout = 10000) {
+  return new BatchStream(batchSize, timeout);
+};
---
+++ images/clickhouse-log-exporter/1.1.0/utils/elastic-client.mjs
@@ -0,0 +1,98 @@
+import axios from 'axios';
+import { Client } from '@elastic/elasticsearch';
+import fs from 'fs';
+
+export class ElasticClient {
+  /**
+   * @param {string} node
+   * @param {{ username: string, password: string }} auth
+   * @param {string} certPath
+   */
+  constructor(node, auth, certPath) {
+    this.config = {
+      node,
+      auth,
+      certPath,
+    };
+  }
+
+  async connect() {
+    const { node, auth } = this.config;
+    const cert = await this._loadKey();
+    const tls = {
+      ca: cert,
+      rejectUnauthorized: true,
+    };
+
+    this.client = new Client({ node, auth, tls });
+  }
+
+  async indexExists(index) {
+    const response = await this.client.indices.exists({
+      index,
+    });
+
+    return response;
+  }
+
+  async streamWrite(index, datasource, _indexSelector) {
+    const indexSelector =
+      _indexSelector ||
+      function () {
+        return { index: { _index: index } };
+      };
+
+    return this.client.helpers.bulk({
+      datasource,
+      flushInterval: 10000,
+      refreshOnCompletion: true,
+      onDocument: indexSelector,
+    });
+  }
+
+  async getProgress(index, clusterId) {
+    try {
+      const progress = await this.client.get({
+        index,
+        id: clusterId,
+      });
+
+      return progress._source;
+    } catch (e) {
+      if (e.meta && e.meta.statusCode !== 404) {
+        throw e;
+      }
+    }
+  }
+
+  async setProgress(index, clusterId, document) {
+    try {
+      return await this.client.index({
+        index,
+        id: clusterId,
+        refresh: true,
+        document,
+      });
+    } catch (e) {
+      if (e.meta && e.meta.statusCode !== 404) {
+        throw e;
+      }
+    }
+  }
+
+  async _loadKey() {
+    const { certPath } = this.config;
+
+    if (fs.existsSync(certPath)) {
+      return fs.promises.readFile(certPath, 'utf-8');
+    }
+
+    const req = await axios.get(certPath, {
+      validateStatus: function (status) {
+        return status < 400;
+      },
+    });
+
+    return await req.data;
+  }
+}
