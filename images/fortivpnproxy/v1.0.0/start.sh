ab0a8bf52ffca50faab48adbf66df5bf54e9e35b
Aleksey Makarov <a.makarov@kazanexpress.tech>
2023-10-13 11:49:59 +0300

    feat: add FortiVPN client with proxy INFR-875

--- images/fortivpnproxy/v1.0.0/start.sh
+++ images/fortivpnproxy/v1.0.0/start.sh
@@ -6,11 +6,21 @@ nginx
 echo Run vpn
 
 if [[ -z "${VPN_PROXY_SCRIPT}" ]] ; then
-    echo "Start vpn without proxy"
-    echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --protocol=fortinet --passwd-on-stdin;
+
+
+    while true
+        do
+            echo "Start vpn without proxy"
+            echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --protocol=fortinet --passwd-on-stdin;
+            if [[  $VPN_PROXY_LOOP = "true" ]] ; then echo "Reconnect" ; else break ; fi
+        done
 else 
-    echo "Start vpn with proxy"
-    echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --protocol=fortinet --script-tun --script "$VPN_PROXY_SCRIPT"  --passwd-on-stdin;
+    while true
+        do
+            echo "Start vpn with proxy"
+            echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --protocol=fortinet --script-tun --script "$VPN_PROXY_SCRIPT"  --passwd-on-stdin;
+            if [[  $VPN_PROXY_LOOP = "true" ]] ; then echo "Reconnect" ; else break ; fi
+        done
 fi
 
 exit 1
